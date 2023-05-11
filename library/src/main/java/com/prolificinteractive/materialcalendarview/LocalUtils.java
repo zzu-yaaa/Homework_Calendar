e051519ba0d91d5ed23c11dffc9643e2518208a8
JUYEON LEE <jy122032@gmail.com>
2023-05-12 02:10:32 +0900

    #1 환경설정

---
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/LocalUtils.java
@@ -0,0 +1,16 @@
+package com.prolificinteractive.materialcalendarview;
+
+import androidx.core.text.TextUtilsCompat;
+import androidx.core.view.ViewCompat;
+
+import java.util.Locale;
+
+class LocalUtils {
+
+  private LocalUtils() { }
+
+  static boolean isRTL() {
+    return TextUtilsCompat.getLayoutDirectionFromLocale(Locale.getDefault())
+        == ViewCompat.LAYOUT_DIRECTION_RTL;
+  }
+}
